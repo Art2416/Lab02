c2d4fd3cc5ccb6995fa336e7d8cc2aa58fdba74b
unknown <cristian.rodriguez-g@labinfo.is.escuelaing.edu.co>
2022-09-01 17:09:10 -0500

    Actualizacion

--- src/main/java/edu/eci/cvds/App.java
+++ src/main/java/edu/eci/cvds/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello David Narvaez!" );
+        System.out.println( "Hello David Narvaez and Cristian Rodríguez!" );
     }
 }
